ea20a0efde4c345227cc127080194aff861fc004
Stefan Blos <stefan.blos@gmail.com>
2019-03-25 12:45:49 +0100

    added second day of closure learning to notes in playground

--- Playgrounds/closures.playground/Contents.swift
+++ Playgrounds/closures.playground/Contents.swift
@@ -43,3 +43,70 @@ say {
 }
 
 // NOTE: if there are no other parameters as in the case above we do not even need parenthesis () after say, but only in the case of no other parameters
+
+// if we want closures to take parameters we write it in the following way:
+func letsTravelTo(action: (String) -> Void) {
+    print("I will now go to:")
+    action("San Francisco")
+    print("Yeah, I'm there!")
+}
+
+letsTravelTo { (place: String) in
+    print("The beautiful town of \(place)")
+}
+
+// just like handing in parameters we can also make closures return parameters of any type of data by replacing the -> Void with the desired data type
+
+func makeTravelPlans(action: (String) -> String) {
+    print("I have a new plan for my next vacation")
+    let destination = action("Hawaii")
+    print(destination)
+    print("Let's check the flights")
+}
+
+makeTravelPlans { (location: String) -> String in
+    return "Next trip will be to \(location)"
+}
+
+// we can use shorthand parameter names in closures since Swift knows the type of arguments that are given to a closure and we can access them in order with a $ sign before, so e.g. $0. Also as Swift knows we are returning a String (in this case) we can omit the return keyword in case we only have one line in our closure
+makeTravelPlans {
+    "Next trip will be to \($0)"
+}
+
+// for multiple parameters in a single closure we can simply put them together using commas
+func travelWithSpeed(action: (String, Int) -> String) {
+    print("Let's go")
+    let description = action("Maui", 80)
+    print(description)
+    print("We're there!")
+}
+
+travelWithSpeed {
+    "We're going to \($0) with the insane speed of \($1) km/h"
+}
+
+// we can also return closures from functions which looks a little goofy because we use two ->'s:
+func goTo() -> (String) -> Void {
+    return {
+        print("I'm going to \($0)")
+    }
+}
+
+let goingTo = goTo()
+goingTo("Spain")
+
+// we can capture values inside the closure. This means we can use values from a function that contains the closure and access them inside of the closure. This sounds complicated but the following example will make it more clear:
+func travelWithCount() -> (String) -> Int {
+    var counter = 0
+    return {
+        print("I'm going to \($0)")
+        counter += 1
+        return counter
+    }
+}
+
+let result = travelWithCount()
+print(result("London"))     // 1
+print(result("Berlin"))     // 2
+print(result("Paris"))      // 3
+
