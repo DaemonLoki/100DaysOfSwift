84c354ede0869318ee039239dfbdbdaac5c01e7f
Stefan Blos <stefan.blos@gmail.com>
2019-04-01 09:20:08 +0200

    added flag images + created basic UI

--- Guess the Flag/Guess the Flag/ViewController.swift
+++ Guess the Flag/Guess the Flag/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
