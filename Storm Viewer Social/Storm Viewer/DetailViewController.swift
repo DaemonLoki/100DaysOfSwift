0b431d3448bae899e807cfde8c195a301d22f938
Stefan Blos <stefan.blos@gmail.com>
2019-04-02 22:32:50 +0200

    added copy of storm viewer as project 3

---
+++ Storm Viewer Social/Storm Viewer/DetailViewController.swift
@@ -0,0 +1,44 @@
+//
+//  DetailViewController.swift
+//  Storm Viewer
+//
+//  Created by Stefan Blos on 31.03.19.
+//  Copyright © 2019 Stefan Blos. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    var selectedImage: String?
+    var imagePosition: Int?
+    var numberOfImages: Int?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.largeTitleDisplayMode = .never
+        
+        if let pos = imagePosition, let num = numberOfImages {
+            title = "Picture \(pos) of \(num)"
+        } else {
+            title = selectedImage ?? "Beautiful image"
+        }
+
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+}
---
+++ Storm Viewer Social/Storm Viewer/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Storm Viewer
+//
+//  Created by Stefan Blos on 31.03.19.
+//  Copyright © 2019 Stefan Blos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        pictures.sort()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let detailVC = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            detailVC.selectedImage = pictures[indexPath.row]
+            detailVC.imagePosition = indexPath.row + 1
+            detailVC.numberOfImages = pictures.count
+            navigationController?.pushViewController(detailVC, animated: true)
+        }
+    }
+
+}
+
