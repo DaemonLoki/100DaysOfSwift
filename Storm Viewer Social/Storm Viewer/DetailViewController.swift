fd0fdc247dbd84c8f0de4238492919b83f4053cc
Stefan Blos <stefan.blos@gmail.com>
2019-04-02 22:52:20 +0200

    added share button with functionality to storm viewer social

--- Storm Viewer Social/Storm Viewer/DetailViewController.swift
+++ Storm Viewer Social/Storm Viewer/DetailViewController.swift
@@ -26,6 +26,8 @@ class DetailViewController: UIViewController {
         } else {
             title = selectedImage ?? "Beautiful image"
         }
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
 
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
@@ -40,5 +42,16 @@ class DetailViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
 }
