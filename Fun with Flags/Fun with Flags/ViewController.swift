53ad6367ef087c2dbe491c3ad31af245595462a1
Stefan Blos <stefan.blos@gmail.com>
2019-04-03 12:18:51 +0200

    added basic project for milestone 1, day 23

---
+++ Fun with Flags/Fun with Flags/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fun with Flags
+//
+//  Created by Stefan Blos on 03.04.19.
+//  Copyright © 2019 Stefan Blos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
