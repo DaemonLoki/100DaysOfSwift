4d9b01239926c22c18b08b3e7f678ae930ed4b7b
Stefan Blos <stefan.blos@gmail.com>
2019-04-04 10:14:47 +0200

    (Day 24) created new project for easy browser

---
+++ Easy Browser/Easy Browser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Easy Browser
+//
+//  Created by Stefan Blos on 04.04.19.
+//  Copyright © 2019 Stefan Blos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
