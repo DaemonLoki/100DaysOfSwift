67c7a49427f5ec4f44f3c418860516949a55e8e9
Stefan Blos <stefan.blos@gmail.com>
2019-04-05 22:23:50 +0200

    (Day 27) added UIAlertController to enter a word

--- Word Scramble/Word Scramble/ViewController.swift
+++ Word Scramble/Word Scramble/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        
         if let url = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let textString = try? String(contentsOf: url) {
                 allWords = textString.components(separatedBy: "\n")
@@ -45,6 +47,24 @@ class ViewController: UITableViewController {
         return cell
     }
 
+    @objc func promptForAnswer() {
+        let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        
+        ac.addTextField()
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak ac] (action) in
+            guard let answer = ac?.textFields?[0].text else {
+                return
+            }
+            self?.submit(answer)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func submit(_ answer: String) {
+        
+    }
 
 }
 
