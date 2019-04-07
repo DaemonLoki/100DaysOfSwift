d1d83cba31ae6ee908f91c264a1d8aa1d3d06f50
Stefan Blos <stefan.blos@gmail.com>
2019-04-07 14:06:04 +0200

    (Day 29) solved challenges and fixed the bug

--- Word Scramble/Word Scramble/ViewController.swift
+++ Word Scramble/Word Scramble/ViewController.swift
@@ -18,6 +18,8 @@ class ViewController: UITableViewController {
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
         
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(startGame))
+        
         if let url = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let textString = try? String(contentsOf: url) {
                 allWords = textString.components(separatedBy: "\n")
@@ -31,7 +33,7 @@ class ViewController: UITableViewController {
         startGame()
     }
     
-    func startGame() {
+    @objc func startGame() {
         title = allWords.randomElement()
         usedWords.removeAll(keepingCapacity: true)
         tableView.reloadData()
@@ -65,9 +67,6 @@ class ViewController: UITableViewController {
     func submit(_ answer: String) {
         let lowerAnswer = answer.lowercased()
         
-        let errorTitle: String
-        let errorMessage: String
-        
         if isPossible(word: lowerAnswer) {
             if isOriginal(word: lowerAnswer) {
                 if isReal(word: lowerAnswer) {
@@ -77,20 +76,19 @@ class ViewController: UITableViewController {
                     tableView.insertRows(at: [indexPath], with: .automatic)
                     return
                 } else {
-                    errorTitle = "Word not recognized"
-                    errorMessage = "You can't just make them up, you know!"
+                    showErrorMessage(title: "Word not recognized", message: "You can't just make them up, you know!")
                 }
             } else {
-                errorTitle = "Word used already"
-                errorMessage = "You can't use words more than once. Come on, creativity!"
+                showErrorMessage(title: "Word used already", message: "You can't use words more than once. Come on, creativity!")
             }
         } else {
             guard let title = title?.lowercased() else { return }
-            errorTitle = "Word not possible"
-            errorMessage = "You can't spell that word from \(title)"
+            showErrorMessage(title: "Word not possible", message: "You can't spell that word from \(title)")
         }
-        
-        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+    }
+    
+    func showErrorMessage(title: String, message: String) {
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .default))
         present(ac, animated: true)
     }
@@ -110,11 +108,24 @@ class ViewController: UITableViewController {
     }
     
     func isOriginal(word: String) -> Bool {
-        // TODO error as userWords does not contain lowercased() words
-        return !usedWords.contains(word)
+        for usedWord in usedWords {
+            if usedWord.lowercased() == word {
+                return false
+            }
+        }
+        return true
     }
     
     func isReal(word: String) -> Bool {
+        
+        if word.count < 3 {
+            return false
+        }
+        
+        if let title = title, title.lowercased() == word {
+            return false
+        }
+        
         let checker = UITextChecker()
         let range = NSRange(location: 0, length: word.utf16.count)
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
