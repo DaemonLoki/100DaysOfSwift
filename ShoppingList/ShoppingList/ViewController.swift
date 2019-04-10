352dc53c8827f62b25fad6312fef4c53c9b3759c
Stefan Blos <stefan.blos@gmail.com>
2019-04-10 13:02:21 +0200

    (Day 32) created initial project

---
+++ ShoppingList/ShoppingList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShoppingList
+//
+//  Created by Stefan Blos on 10.04.19.
+//  Copyright © 2019 Stefan Blos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
