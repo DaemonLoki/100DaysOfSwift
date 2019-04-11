ca0478944c3809aa008240da1a75c211fe3a8a84
Stefan Blos <stefan.blos@gmail.com>
2019-04-11 13:16:34 +0200

    (Day 33) added structs conforming to codable and json parsing to show content

---
+++ Whitehouse Petitions/Whitehouse Petitions/Petitions.swift
@@ -0,0 +1,10 @@
+//
+//  Petitions.swift
+//  Whitehouse Petitions
+//
+//  Created by Stefan Blos on 11.04.19.
+//  Copyright © 2019 Stefan Blos. All rights reserved.
+//
+struct Petitions: Codable {
+    var results: [Petition]
+}
