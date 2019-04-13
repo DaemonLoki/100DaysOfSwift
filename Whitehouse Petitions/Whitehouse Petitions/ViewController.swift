026dd3ecda5dc9bbe57450078cdec52a7e1df4d6
Stefan Blos <stefan.blos@gmail.com>
2019-04-13 14:49:24 +0200

    (Day 35) completed first 2 challenges

--- Whitehouse Petitions/Whitehouse Petitions/ViewController.swift
+++ Whitehouse Petitions/Whitehouse Petitions/ViewController.swift
@@ -10,12 +10,14 @@ import UIKit
 
 class ViewController: UITableViewController {
     
-    var petitions = [Petition]()
+    var shownPetitions = [Petition]()
+    var allPetitions = [Petition]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let urlString: String
+        title = "Petitions"
             
         if navigationController?.tabBarItem.tag == 0 {
             // let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
@@ -25,6 +27,11 @@ class ViewController: UITableViewController {
             urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
         }
         
+        navigationItem.rightBarButtonItems =
+            [UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(showSearch)),
+             UIBarButtonItem(title: "Credits", style: .plain, target: self, action: #selector(showCredits))]
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Show all", style: .plain, target: self, action: #selector(showAllPetitions))
         
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
@@ -33,6 +40,36 @@ class ViewController: UITableViewController {
             }
         }
         showError()
+    }
+    
+    @objc func showSearch() {
+        let ac = UIAlertController(title: "Search for:", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        ac.addAction(UIAlertAction(title: "Cancel", style: .destructive))
+        ac.addAction(UIAlertAction(title: "Go", style: .default, handler: { [weak self, weak ac] (action) in
+            guard let searchTerm = ac?.textFields?[0].text else {
+                return
+            }
+            self?.performSearchFor(term: searchTerm)
+        }))
+        present(ac, animated: true)
+    }
+    
+    @objc func showAllPetitions() {
+        shownPetitions = allPetitions
+        tableView.reloadData()
+    }
+    
+    @objc func showCredits() {
+        let ac = UIAlertController(title: "Credits", message: "All presented data comes from the 'We The People' API of the Whitehouse", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    func performSearchFor(term: String) {
+        shownPetitions = allPetitions.filter( {$0.title.contains(term) || $0.body.contains(term) } )
+        tableView.reloadData()
     }
     
     func showError() {
@@ -45,18 +82,18 @@ class ViewController: UITableViewController {
         let decoder = JSONDecoder()
         
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
-            petitions = jsonPetitions.results
-            tableView.reloadData()
+            allPetitions = jsonPetitions.results
+            showAllPetitions()
         }
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return petitions.count
+        return shownPetitions.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let petition = petitions[indexPath.row]
+        let petition = shownPetitions[indexPath.row]
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
         return cell
@@ -64,7 +101,7 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let vc = DetailViewController()
-        vc.detailItem = petitions[indexPath.row]
+        vc.detailItem = shownPetitions[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
     }
 }
