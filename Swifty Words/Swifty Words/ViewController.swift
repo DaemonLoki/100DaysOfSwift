f16a456f77d498f7cab650389cf822cabe082730
Stefan Blos <stefan.blos@gmail.com>
2019-04-14 12:47:28 +0200

    (Day 36) Created project sceleton

---
+++ Swifty Words/Swifty Words/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swifty Words
+//
+//  Created by Stefan Blos on 14.04.19.
+//  Copyright © 2019 Stefan Blos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
