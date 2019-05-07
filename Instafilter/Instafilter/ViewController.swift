46c593629953e7e6a2790eae201b06917e27d70e
Stefan Blos <stefan.blos@gmail.com>
2019-05-07 11:06:01 +0200

    (Day 52) created Instafilter project and added interface to it

---
+++ Instafilter/Instafilter/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Instafilter
+//
+//  Created by Stefan Blos on 07.05.19.
+//  Copyright © 2019 Stefan Blos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
+    
+}
+
