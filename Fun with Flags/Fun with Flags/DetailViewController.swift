348978575acab571d3c522c4bec4acfcb9806f42
Stefan Blos <stefan.blos@gmail.com>
2019-05-15 11:16:54 +0200

    (Day 58) Challenge 3: scaling image on tap in Fun with Flags

--- Fun with Flags/Fun with Flags/DetailViewController.swift
+++ Fun with Flags/Fun with Flags/DetailViewController.swift
@@ -21,21 +21,34 @@ class DetailViewController: UIViewController {
         
         if let country = country {
             imageView.image = UIImage(named: country.lowercased())
+            let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(playImageAnimaition(recognizer:)))
+            imageView.isUserInteractionEnabled = true
+            imageView.addGestureRecognizer(tapGestureRecognizer)
         }
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareClicked))
     }
     
+    @objc func playImageAnimaition(recognizer: UITapGestureRecognizer) {
+        UIView.animate(withDuration: 0.2, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 10, options: [], animations: {
+            self.imageView.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
+        }) { (true) in
+            UIView.animate(withDuration: 0.2, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 2, options: [], animations: {
+                self.imageView.transform = .identity
+            })
+        }
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.navigationBar.prefersLargeTitles = false
-        navigationController?.hidesBarsOnTap = true
+        //navigationController?.hidesBarsOnTap = true
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.navigationBar.prefersLargeTitles = true
-        navigationController?.hidesBarsOnTap = false
+        //navigationController?.hidesBarsOnTap = false
     }
 
     @objc func shareClicked() {
