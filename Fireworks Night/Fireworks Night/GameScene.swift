007916e5f967eebdc3dae8ea8f48e6c6bf506e9b
Stefan Blos <stefan.blos@gmail.com>
2019-06-13 13:39:17 +0200

    (Day 70) setup project

---
+++ Fireworks Night/Fireworks Night/GameScene.swift
@@ -0,0 +1,26 @@
+//
+//  GameScene.swift
+//  Fireworks Night
+//
+//  Created by Stefan Blos on 13.06.19.
+//  Copyright © 2019 Stefan Blos. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    override func didMove(to view: SKView) {
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
