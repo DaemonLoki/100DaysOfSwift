3ff0fdba61516aa2adbe63c00720be8a7be26162
Stefan Blos <stefan.blos@gmail.com>
2019-08-05 12:45:19 +0200

    (Day 83) finished media part with CollectionView and ImagePicker

--- Selfie Share/Selfie Share/ViewController.swift
+++ Selfie Share/Selfie Share/ViewController.swift
@@ -8,13 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController {
+    
+    var images = [UIImage]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Selfie Share"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importPicture))
+    }
+    
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
     }
 
+}
 
+extension ViewController {
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageView", for: indexPath)
+        
+        if let imageView = cell.viewWithTag(1000) as? UIImageView {
+            imageView.image = images[indexPath.row]
+        }
+        
+        return cell
+    }
+    
+}
+
+extension ViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+ 
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        
+        dismiss(animated: true)
+        
+        images.insert(image, at: 0)
+        collectionView.reloadData()
+    }
+    
 }
 
