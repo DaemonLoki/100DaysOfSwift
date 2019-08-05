6abec66352f01baf2ba79ea33e1ea96087b9746d
Stefan Blos <stefan.blos@gmail.com>
2019-08-05 12:21:07 +0200

    (Day 83) set up project Selfie Share

---
+++ Selfie Share/Selfie Share/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Selfie Share
+//
+//  Created by Stefan Blos on 05.08.19.
+//  Copyright © 2019 Stefan Blos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
