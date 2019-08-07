ee12d55b54ed549af711edb16b04d519bb277862
Stefan Blos <stefan.blos@gmail.com>
2019-08-07 23:30:04 +0200

    (Day 85) added code to create wall and vortex nodes

--- Marble Maze/Marble Maze/GameScene.swift
+++ Marble Maze/Marble Maze/GameScene.swift
@@ -9,11 +9,78 @@
 import SpriteKit
 import GameplayKit
 
+enum CollisionTypes: UInt32 {
+    case player = 1
+    case wall = 2
+    case star = 4
+    case vortex = 8
+    case finish = 16
+}
+
 class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    func loadLevel() {
+        guard let levelUrl = Bundle.main.url(forResource: "level1", withExtension: "txt") else {
+            fatalError("Could not find level1.txt in the app bundle.")
+        }
+        
+        guard let levelString = try? String(contentsOf: levelUrl) else {
+            fatalError("Could not load level1.txt from the app bundle.")
+        }
+        
+        let lines = levelString.components(separatedBy: "\n")
+        
+        for (row, line) in lines.reversed().enumerated() {
+            for (column, letter) in line.enumerated() {
+                let position = CGPoint(x: (64 * column) + 32, y: (64 * row) + 32)
+                
+                if letter == "x" {
+                    let wallNode = createWallNodeAt(position)
+                    addChild(wallNode)
+                } else if letter == "v" {
+                    // load vortex
+                    let vortexNode = createVortexNodeAt(position)
+                    addChild(vortexNode)
+                } else if letter == "s" {
+                    // load star
+                } else if letter == "f" {
+                    // load finish
+                } else if letter == " " {
+                    // empty space, do nothing
+                } else {
+                    fatalError("Unknown level letter: \(letter)")
+                }
+            }
+        }
+    }
+    
+    func createWallNodeAt(_ position: CGPoint) -> SKSpriteNode {
+        let node = SKSpriteNode(imageNamed: "block")
+        node.position = position
+        
+        node.physicsBody = SKPhysicsBody(rectangleOf: node.size)
+        node.physicsBody?.categoryBitMask = CollisionTypes.wall.rawValue
+        node.physicsBody?.isDynamic = false
+        return node
+    }
+    
+    func createVortexNodeAt(_ position: CGPoint) -> SKSpriteNode {
+        let node = SKSpriteNode(imageNamed: "vortex")
+        node.name = "vortex"
+        node.position = position
+        node.run(SKAction.repeatForever(SKAction.rotate(byAngle: .pi, duration: 1)))
+        node.physicsBody = SKPhysicsBody(circleOfRadius: node.size.width / 2)
+        node.physicsBody?.isDynamic = false
+        
+        node.physicsBody?.categoryBitMask = CollisionTypes.vortex.rawValue
+        node.physicsBody?.contactTestBitMask = CollisionTypes.player.rawValue
+        node.physicsBody?.collisionBitMask = 0
+        return node
     }
 }
