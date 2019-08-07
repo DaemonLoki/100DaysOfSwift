a1d1f469b8528353521647b9fb64aacd4c415377
Stefan Blos <stefan.blos@gmail.com>
2019-08-07 23:05:19 +0200

    (Day 85) basic project setup

---
+++ Marble Maze/Marble Maze/GameScene.swift
@@ -0,0 +1,19 @@
+//
+//  GameScene.swift
+//  Marble Maze
+//
+//  Created by Stefan Blos on 07.08.19.
+//  Copyright © 2019 Stefan Blos. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+}
