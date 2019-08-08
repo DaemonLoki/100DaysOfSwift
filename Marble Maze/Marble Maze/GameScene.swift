c4154fdef82b13a970ebc14774ed4f3a306bf2e3
Stefan Blos <stefan.blos@gmail.com>
2019-08-08 15:33:13 +0200

    (Day 86) added player ball to the project

--- Marble Maze/Marble Maze/GameScene.swift
+++ Marble Maze/Marble Maze/GameScene.swift
@@ -19,6 +19,8 @@ enum CollisionTypes: UInt32 {
 
 class GameScene: SKScene {
     
+    var player: SKSpriteNode!
+    
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "background.jpg")
         background.position = CGPoint(x: 512, y: 384)
@@ -27,9 +29,28 @@ class GameScene: SKScene {
         addChild(background)
         
         loadLevel()
+        
+        physicsWorld.gravity = .zero
+        
+        createPlayer()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    func createPlayer() {
+        player = SKSpriteNode(imageNamed: "player")
+        player.position = CGPoint(x: 96, y: 672)
+        player.zPosition = 1
+        player.physicsBody = SKPhysicsBody(circleOfRadius: player.size.width / 2)
+        player.physicsBody?.allowsRotation = false
+        player.physicsBody?.linearDamping = 0.5
+        
+        player.physicsBody?.categoryBitMask = CollisionTypes.player.rawValue
+        player.physicsBody?.contactTestBitMask = CollisionTypes.star.rawValue | CollisionTypes.vortex.rawValue | CollisionTypes.finish.rawValue
+        player.physicsBody?.collisionBitMask = CollisionTypes.wall.rawValue
+        
+        addChild(player)
     }
     
     func loadLevel() {
