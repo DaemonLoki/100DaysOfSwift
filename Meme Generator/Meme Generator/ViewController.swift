8b4d677ece26c2f1f0ce2a4c6a9e8ff388564660
Stefan Blos <stefan.blos@gmail.com>
2019-10-28 09:58:42 +0100

    (Day 90) created UI for Meme Generator

---
+++ Meme Generator/Meme Generator/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Meme Generator
+//
+//  Created by Stefan Blos on 28.10.19.
+//  Copyright © 2019 Stefan Blos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func importPicture(_ sender: Any) {
+    }
+    
+    @IBAction func setTopText(_ sender: Any) {
+    }
+    
+    @IBAction func setBottomText(_ sender: Any) {
+    }
+    
+}
+
