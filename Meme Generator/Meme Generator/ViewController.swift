80f5a891e69fed7b0e42d59fc8ffb39f06431aa9
Stefan Blos <stefan.blos@gmail.com>
2019-10-28 10:49:24 +0100

    (Day 90) added text drawing and input as well as share functionality

--- Meme Generator/Meme Generator/ViewController.swift
+++ Meme Generator/Meme Generator/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         title = "Meme Generator"
         
-        navigationController?.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareImage))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareImage))
     }
 
     @IBAction func importPicture(_ sender: Any) {
@@ -32,13 +32,22 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     @IBAction func setTopText(_ sender: Any) {
+        askForText(at: "top") { [weak self] text in
+            self?.topText = text
+        }
     }
     
     @IBAction func setBottomText(_ sender: Any) {
+        askForText(at: "bottom") { [weak self] text in
+            self?.bottomText = text
+        }
     }
     
     @objc func shareImage() {
-        
+        guard let imageToShare = imageView.image else { return }
+        let itemsToShare = [imageToShare]
+        let ac = UIActivityViewController(activityItems: itemsToShare, applicationActivities: nil)
+        present(ac, animated: true)
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
@@ -50,12 +59,48 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         drawMeme()
     }
     
+    func askForText(at position: String, action: @escaping (_ selectedText: String) -> ()) {
+        let ac = UIAlertController(title: "Enter text to be shown at the \(position):", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let showAction = UIAlertAction(title: "Show", style: .default) { [unowned ac, weak self] _ in
+            guard let textField = ac.textFields?[0] else { return }
+            guard let text = textField.text else { return }
+            
+            action(text)
+            self?.drawMeme()
+        }
+        
+        ac.addAction(UIAlertAction(title: "Cancel", style: .destructive))
+        ac.addAction(showAction)
+        
+        present(ac, animated: true)
+    }
+    
     func drawMeme() {
         guard let selectedImage = selectedImage else { return }
         let renderer = UIGraphicsImageRenderer(size: selectedImage.size)
         
         let image = renderer.image { (ctx) in
             selectedImage.draw(at: CGPoint(x: 0, y: 0))
+            
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.alignment = .center
+            
+            let attrs: [NSMutableAttributedString.Key: Any] = [
+                .font: UIFont.systemFont(ofSize: 36),
+                .paragraphStyle: paragraphStyle
+            ]
+            
+            if !topText.isEmpty {
+                let attributedTopText = NSMutableAttributedString(string: topText, attributes: attrs)
+                attributedTopText.draw(with: CGRect(x: 0, y: 20, width: selectedImage.size.width, height: 40), options: .usesLineFragmentOrigin, context: nil)
+            }
+            
+            if !bottomText.isEmpty {
+                let attributedBottomText = NSMutableAttributedString(string: bottomText, attributes: attrs)
+                attributedBottomText.draw(with: CGRect(x: 0, y: selectedImage.size.height - 60, width: selectedImage.size.width, height: 40), options: .usesLineFragmentOrigin, context: nil)
+            }
         }
         
         imageView.image = image
