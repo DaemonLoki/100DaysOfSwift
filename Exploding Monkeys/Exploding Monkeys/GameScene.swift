7394e13926da7b10b0932787c00179de3a2d9bee
Stefan Blos <stefan.blos@gmail.com>
2019-11-03 08:59:24 +0100

    (Day 94) added creation of buildings

---
+++ Exploding Monkeys/Exploding Monkeys/GameScene.swift
@@ -0,0 +1,51 @@
+//
+//  GameScene.swift
+//  Exploding Monkeys
+//
+//  Created by Stefan Blos on 03.11.19.
+//  Copyright © 2019 Stefan Blos. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+enum CollisionTypes: UInt32 {
+    case banana = 1
+    case building = 2
+    case player = 4
+}
+
+class GameScene: SKScene {
+    
+    var buildings = [BuildingNode]()
+    
+    override func didMove(to view: SKView) {
+        backgroundColor = UIColor(hue: 0.669, saturation: 0.99, brightness: 0.67, alpha: 1)
+        
+        createBuildings()
+    }
+    
+    func createBuildings() {
+        var currentX: CGFloat = -15
+        
+        while currentX < 1024 {
+            let size = CGSize(width: Int.random(in: 2...4) * 40, height: Int.random(in: 300...600))
+            currentX += size.width + 2
+            
+            let building = BuildingNode(color: UIColor.red, size: size)
+            building.position = CGPoint(x: currentX - (size.width / 2), y: size.height / 2)
+            building.setup()
+            addChild(building)
+            
+            buildings.append(building)
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
